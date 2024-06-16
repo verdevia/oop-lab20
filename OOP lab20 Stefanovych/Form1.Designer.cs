1b9c9ea599aa6568a78f2d6ff3bfdca18ce5228f
verdevia <146423357+verdevia@users.noreply.github.com>
2024-06-16 20:26:51 +0300

    Add files via upload

---
+++ OOP lab20 Stefanovych/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace lab20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.x1TextBox = new System.Windows.Forms.TextBox();
+            this.y1TextBox = new System.Windows.Forms.TextBox();
+            this.ATextBox = new System.Windows.Forms.TextBox();
+            this.BTextBox = new System.Windows.Forms.TextBox();
+            this.CTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // x1TextBox
+            // 
+            this.x1TextBox.Location = new System.Drawing.Point(29, 25);
+            this.x1TextBox.Name = "x1TextBox";
+            this.x1TextBox.Size = new System.Drawing.Size(100, 20);
+            this.x1TextBox.TabIndex = 0;
+            // 
+            // y1TextBox
+            // 
+            this.y1TextBox.Location = new System.Drawing.Point(155, 24);
+            this.y1TextBox.Name = "y1TextBox";
+            this.y1TextBox.Size = new System.Drawing.Size(100, 20);
+            this.y1TextBox.TabIndex = 1;
+            // 
+            // ATextBox
+            // 
+            this.ATextBox.Location = new System.Drawing.Point(29, 65);
+            this.ATextBox.Name = "ATextBox";
+            this.ATextBox.Size = new System.Drawing.Size(100, 20);
+            this.ATextBox.TabIndex = 2;
+            // 
+            // BTextBox
+            // 
+            this.BTextBox.Location = new System.Drawing.Point(155, 64);
+            this.BTextBox.Name = "BTextBox";
+            this.BTextBox.Size = new System.Drawing.Size(100, 20);
+            this.BTextBox.TabIndex = 3;
+            // 
+            // CTextBox
+            // 
+            this.CTextBox.Location = new System.Drawing.Point(280, 64);
+            this.CTextBox.Name = "CTextBox";
+            this.CTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CTextBox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Розрахувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(152, 115);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CTextBox);
+            this.Controls.Add(this.BTextBox);
+            this.Controls.Add(this.ATextBox);
+            this.Controls.Add(this.y1TextBox);
+            this.Controls.Add(this.x1TextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox x1TextBox;
+        private System.Windows.Forms.TextBox y1TextBox;
+        private System.Windows.Forms.TextBox ATextBox;
+        private System.Windows.Forms.TextBox BTextBox;
+        private System.Windows.Forms.TextBox CTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ OOP lab20 Stefanovych/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double x1 = double.Parse(x1TextBox.Text);
+                double y1 = double.Parse(y1TextBox.Text);
+                double A = double.Parse(ATextBox.Text);
+                double B = double.Parse(BTextBox.Text);
+                double C = double.Parse(CTextBox.Text);
+
+                double distance = DistanceFromPointToLine(x1, y1, A, B, C);
+
+                resultLabel.Text = "Відстань: " + distance.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введіть числові значення у всі поля.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private double DistanceFromPointToLine(double x1, double y1, double A, double B, double C)
+        {
+            double numerator = Math.Abs(A * x1 + B * y1 + C);
+            double denominator = Math.Sqrt(A * A + B * B);
+            double distance = numerator / denominator;
+            return distance;
+        }
+
+    }
+}
